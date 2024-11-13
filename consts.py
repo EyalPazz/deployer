63f91e1dbd0359dc885540fe319398c245ee2c87
EyalPazz <eyalp700@gmail.com>
2024-11-13 11:32:04 +0200

    feat: add consts

---
+++ consts.py
@@ -0,0 +1,15 @@
+# PROMPTS
+LINE_DECORATOR = "=========="
+ADD_NEW_OF_FIELD = "Add a new"
+ADD_NEW_KV = "Add a key-value pair"
+ENTER_KEY = "Enter key for"
+ENTER_VALUE = "Enter value for"
+ADD_ANOTHER_KV = "Add another key-value pair?"
+
+# MISC
+ENABLED = 'enabled'
+FALSE = 'false'
+
+# CONFIG
+DEFAULT_AWS_REGION = 'eu-central-1'
+DEFAULT_ENV = 'production'
